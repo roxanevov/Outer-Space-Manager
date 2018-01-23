e3180f5415c5651507f0e358fd67ca27b7e0d9b4
rvovard@formation-cci.fr <rvovard@formation-cci.fr>
2018-01-23 16:02:32 +0100

    add bdd

---
+++ app/src/main/java/vovard/com/outerspacemanager/outerspacemanager/DatabaseUser.java
@@ -0,0 +1,8 @@
+package vovard.com.outerspacemanager.outerspacemanager;
+
+/**
+ * Created by rvovard on 23/01/2018.
+ */
+
+public class DatabaseUser {
+}
---
+++ app/src/main/java/vovard/com/outerspacemanager/outerspacemanager/VueGeneraleActivity.java
@@ -0,0 +1,13 @@
+package vovard.com.outerspacemanager.outerspacemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueGeneraleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vue_generale);
+    }
+}
